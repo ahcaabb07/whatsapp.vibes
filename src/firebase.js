eb4dd7532797ea9980f8b958561732a369aab32b
Joes <veer.youssef@gmail.com>
2020-10-08 13:41:17 +0100

    start + firebase setUp

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaoZ8bxdoH0QDPUPo6hTza9dA6sVpe1XQ",
+  authDomain: "whatsapp-mern-a6125.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-a6125.firebaseio.com",
+  projectId: "whatsapp-mern-a6125",
+  storageBucket: "whatsapp-mern-a6125.appspot.com",
+  messagingSenderId: "935135538740",
+  appId: "1:935135538740:web:43ea73eb1e682d88196461",
+};
